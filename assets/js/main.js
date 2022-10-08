1023fca4a4d952298f30aa85a83e944ee8eec11f
encefis <cusco.cientifico.eventos@gmail.com>
2022-10-08 18:21:22 -0300

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Jan 20, 2022").getTime();
+var target_date = new Date("Jan 25, 2023").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
